957deb1543c2cb152df9709a4266d05a90a39a9c
Scretch-1 <oleg1.plehanov@gmail.com>
2018-09-30 17:04:16 +0500

    commit14

--- bs4/app/js/common.js
+++ bs4/app/js/common.js
@@ -11,6 +11,17 @@ $(function() {
   //  active tooltips BS4
   // $('[data-toggle="tooltip"]').tooltip()
 
+  // ripple effect btn
+  // Добавляем элементу класс .ripple Активируем sass файл в libs
+  // [].map.call(document.querySelectorAll(".ripple"), el=> {
+  // 	el.addEventListener('click',e => {
+  // 		e = e.touches ? e.touches[0] : e;
+  // 		const r = el.getBoundingClientRect(), d = Math.sqrt(Math.pow(r.width,2)+Math.pow(r.height,2)) * 2;
+  // 		el.style.cssText = `--s: 0; --o: 1;`;  el.offsetTop; 
+  // 		el.style.cssText = `--t: 1; --o: 0; --d: ${d}; --x:${e.clientX - r.left}; --y:${e.clientY - r.top};`
+  // 	})
+  // })
+
 	//Chrome Smooth Scroll
 	try {
 		$.browserSelector();
